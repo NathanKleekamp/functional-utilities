7e7bde8e3da9547bfffc460c3e94bb97f1260d6b
Nathan Kleekamp <nkleekamp@gmail.com>
2018-09-08 12:08:36 -0400

    remove parens around test callback param

--- array/rest.spec.js
+++ array/rest.spec.js
@@ -1,19 +1,19 @@
 const { test } = require('tape');
 const { rest } = require('../index');
 
-test('Rest returns an empty array when passed an empty array', (t) => {
+test('Rest returns an empty array when passed an empty array', t => {
   const testArray = [];
   t.deepEqual(rest(testArray), [], 'return empty array');
   t.end();
 });
 
-test('Rest returns an empty array when passed an array with one item', (t) => {
+test('Rest returns an empty array when passed an array with one item', t => {
   const testArray = [1];
   t.deepEqual(rest(testArray), [], 'return empty array');
   t.end();
 });
 
-test('Rest returns an array without the first item', (t) => {
+test('Rest returns an array without the first item', t => {
   const testArray = [1, 2, 3, 4, 5];
   const expected = [2, 3, 4, 5];
   t.deepEqual(rest(testArray), expected, 'returns correct output');
